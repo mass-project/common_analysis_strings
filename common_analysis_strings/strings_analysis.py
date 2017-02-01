23ab01289e8edcdbd5e4b7ff76abc7cddecfbcdc
Timm Behner <behner@fkie.fraunhofer.de>
2017-02-01 16:32:44 +0100

    Different implementation; tests passing; Running time 0.7sec per 1MByte

--- common_analysis_strings/strings_analysis.py
+++ common_analysis_strings/strings_analysis.py
@@ -3,42 +3,22 @@ from common_helper_files import get_directory_for_filename, get_version_string_f
 
 import re
 import string
-from collections import namedtuple
+
+
+class StringMatch:
+    def __init__(self, begin, end, content):
+        self.begin = begin
+        self.end = end
+        self.content = content
+
+    def __repr__(self):
+        return '[{}]: ({}:{}) -> {}'.format(self.__class__.__name__, self.begin, self.end, self.content)
+
 
 system_version = get_version_string_from_git(get_directory_for_filename(__file__))
 
-class StringMatch:
-    def __init__(self, start=0, string=''):
-        self.start = start
-        self.string = string
-
-    def __add__(self, other):
-        if len(self.string) == 0:
-            return StringMatch(other.start, other.string)
-        if len(other.string) == 0:
-            return StringMatch(self.start, self.string)
-
-        if self.start <= other.start:
-            return StringMatch(self.start, self.string + other.string)
-        else:
-            return StringMatch(other.start, other.string + self.string)
-
-    def __len__(self):
-        return len(self.string)
-
-    def get_plain_tuple(self):
-        return (self.start, self.string)
-
-    def shift_start_by(self,offset):
-        self.start += offset
-
-def _check_match(match):
-    if match:
-        return StringMatch(match.start(), match.group().decode('utf-8'))
-    else:
-        return StringMatch(0,'')
-
 default_chunk_size = 64 * 1024 * 1024
+
 
 class CommonAnalysisStrings(AnalysisPluginFile):
     def __init__(self, min_length=4, printable=string.printable, chunk_size=default_chunk_size):
@@ -51,53 +31,52 @@ class CommonAnalysisStrings(AnalysisPluginFile):
         self.string_re = re.compile(printable_pattern.encode('utf-8'))
         self.chunk_size = chunk_size
 
-
     def analyze_file(self, file_path):
         with open(file_path, 'rb') as f:
             result = self.prepare_analysis_report_dictionary()
-            result['found strings'] = self._find_from_file_obj(f)
+            result['found strings'] = [(m.begin, m.content) for m in self._find_from_file_obj(f)]
             return result
 
-    def _handle_begin_of_string(self,data):
-        start_pattern = '[{}]+'.format(self.printable)
-        match = re.match(start_pattern.encode('utf-8'), data)
-        return _check_match(match)
+    def _append_found_string(self, found_strings, mo, offset):
+        if mo:
+            new_str_match = StringMatch(mo.start()+offset, mo.end()+offset, mo.group().decode('utf-8'))
+            found_strings.append(new_str_match)
+        return found_strings
 
-    def _handle_end_of_string(self,data):
-        end_pattern = '[{}]+\Z'.format(self.printable)
-        match = re.search(end_pattern.encode('utf-8'), data)
-        return _check_match(match)
+    def _merge_found_strings(self, found_strings):
+        filtered_findings = []
+        for i in range(len(found_strings)):
+            if found_strings[i].begin == found_strings[i].end:
+                continue
+            for j in range(i+1, len(found_strings)):
+                if found_strings[i].end >= found_strings[j].begin:
+                    start = found_strings[i].end - found_strings[j].begin
+                    found_strings[i].content += found_strings[j].content[start:]
+                    found_strings[i].end = found_strings[j].end
 
-    def _find_from_file_obj(self,file_obj):
+                    found_strings[j].begin = 0
+                    found_strings[j].end = 0
+                    found_strings[j].content = ''
+                else:
+                    break
+            if len(found_strings[i].content) >= self.min_length:
+                filtered_findings.append(found_strings[i])
+        return filtered_findings
+
+    def _find_from_file_obj(self, file_obj):
         found_strings = []
         offset = 0
-        cached = StringMatch()
+        string_at_start = re.compile(r'^[{}]+'.format(self.printable).encode('utf-8'))
+        string_at_end = re.compile(r'[{}]+$'.format(self.printable).encode('utf-8'))
+
         for data in read_in_chunks(file_obj, self.chunk_size):
-            left_boundary = self._handle_begin_of_string(data)
-            left_boundary.shift_start_by(offset)
-
-            right_boundary = self._handle_end_of_string(data)
-            right_boundary.shift_start_by(offset)
-
-            if len(cached + left_boundary) >= self.min_length:
-                match = cached + left_boundary
-                if len(left_boundary) < len(data):
-                    found_strings.append(match.get_plain_tuple())
-                    cached = right_boundary
-                else: 
-                    cached = match
-            else:
-                cached = right_boundary
-
-            data = data[len(left_boundary):len(cached)-1]
+            start_mo = string_at_start.search(data)
+            end_mo = string_at_end.search(data)
+            self._append_found_string(found_strings, start_mo, offset)
+            self._append_found_string(found_strings, end_mo, offset)
 
             for mo in self.string_re.finditer(data):
-                start = offset + mo.start()
-                match = mo.group().decode('utf-8')
-                found_strings.append((start, match))
-            offset += self.chunk_size
+                self._append_found_string(found_strings, mo, offset)
 
-        if len(cached) != 0:
-            found_strings.append(cached.get_plain_tuple())
-
-        return found_strings
+            offset += len(data)
+        return self._merge_found_strings(found_strings)
--- common_analysis_strings/tests/test_strings_analysis.py
+++ common_analysis_strings/tests/test_strings_analysis.py
@@ -1,6 +1,8 @@
 import unittest
 from common_analysis_strings import CommonAnalysisStrings
 from tempfile import NamedTemporaryFile
+import random
+import string
 
 test_chunk_size = 1024
 
@@ -13,7 +15,7 @@ class StringFinderTestCase(unittest.TestCase):
             f.write('Hello, World')
         found_strings = string_finder.analyze_file(text_file.name)['found strings']
         text_file.close()
-        self.assertIn((0,'Hello, World'), found_strings)
+        self.assertIn((0, 'Hello, World'), found_strings)
 
     def test_finds_strings_in_binary(self):
         string_finder = CommonAnalysisStrings(chunk_size=test_chunk_size)
@@ -22,7 +24,7 @@ class StringFinderTestCase(unittest.TestCase):
             f.write(b'\x00\xFFHello\xde\xad\xbe\xef')
         found_strings = string_finder.analyze_file(binary_file.name)['found strings']
         binary_file.close()
-        self.assertIn((2,'Hello'), found_strings)
+        self.assertIn((2, 'Hello'), found_strings)
 
     def test_find_strings_on_file_slice_boundary(self):
         string_finder = CommonAnalysisStrings(chunk_size=test_chunk_size)
@@ -43,7 +45,7 @@ class StringFinderTestCase(unittest.TestCase):
             f.write(b'a'*test_chunk_size*2 + b'b'*100)
         found_strings = string_finder.analyze_file(large_binary_file.name)['found strings']
         large_binary_file.close()
-        is_there = (0,'a'*test_chunk_size*2 + 'b'*100) in found_strings
+        is_there = (0, 'a'*test_chunk_size*2 + 'b'*100) in found_strings
         self.assertEqual(len(found_strings), 1, "There are more found strings than possible.")
         self.assertTrue(is_there, "String not in found_strings.")
 
@@ -54,4 +56,14 @@ class StringFinderTestCase(unittest.TestCase):
             f.write(b'\x00'*test_chunk_size + b'b'*100)
         found_strings = string_finder.analyze_file(large_binary_file.name)['found strings']
         large_binary_file.close()
-        self.assertIn((test_chunk_size,'b'*100), found_strings)
+        self.assertIn((test_chunk_size, 'b'*100), found_strings)
+
+    @unittest.skip('only for performance test')
+    def test_find_strings_in_large_test_file(self):
+        string_finder = CommonAnalysisStrings(chunk_size=test_chunk_size)
+        with NamedTemporaryFile() as tmp:
+            with open(tmp.name, 'wb') as f:
+                for i in range(1024*1024*10):
+                    f.write(random.choice(string.printable).encode('utf-8'))
+            found_strings = string_finder.analyze_file(tmp.name)['found strings']
+            self.assertEqual(0, found_strings[0][0])
